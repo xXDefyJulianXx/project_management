6c511fbfaf4df0b833e3797ce5e7aa178f046317
Daniel Esteban Bustamante Tamayo <danies9818@hotmail.com>
2020-05-14 02:00:19 -0500

    Alcance

---
+++ src/app/components/alcance/alcance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alcance',
+  templateUrl: './alcance.component.html',
+  styleUrls: ['./alcance.component.css']
+})
+export class AlcanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
