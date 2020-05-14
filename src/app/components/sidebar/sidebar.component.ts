8aa663ffc159a6c9f1e45fd3a8f488f005674393
Daniel Esteban Bustamante Tamayo <danies9818@hotmail.com>
2020-05-13 22:38:17 -0500

    Concentrese Juego

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -17,6 +17,7 @@ export const ROUTES: RouteInfo[] = [
     // { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
     // { path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
     { path: '/introduction', title: 'Introducción al curso', icon: 'import_contacts', class: ''},
+    { path: '/module-1', title: 'Project Charter', icon: 'chrome_reader_mode', class: ' ' },
     { path: '/actividad/MODULO_UNO', title: 'Actividad Módulo 1', icon: 'sports_esports', class: ' ' },
     { path: '/module-2', title: 'Grupo de Planeación', icon: 'assignment_turned_in', class: ' ' },
     { path: '/module-3', title: 'Alcance', icon: 'assignment', class: ' ' },
