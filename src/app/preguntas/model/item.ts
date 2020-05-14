f7e0173e148ee9af24d00ec9780fdfd0ae106a3b
Daniel Esteban Bustamante Tamayo <danies9818@hotmail.com>
2020-05-13 22:28:58 -0500

    Concentrese Juego

--- src/app/preguntas/model/item.ts
+++ src/app/preguntas/model/item.ts
@@ -2,4 +2,5 @@ export class Item {
     id: number;
     elemento: string;
     mostrar: boolean;
+    correcto: boolean;
 }
--- src/app/preguntas/preguntas.component.ts
+++ src/app/preguntas/preguntas.component.ts
@@ -1,41 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {PreguntasService} from './preguntas.service';
 import {Pregunta} from './model/pregunta';
 import {Item} from './model/item';
 import {newArray} from '@angular/compiler/src/util';
+import {async} from 'rxjs/internal/scheduler/async';
+import {delay} from 'rxjs/operators';
+import {timer} from 'rxjs';
 
 @Component({
-  selector: 'app-preguntas',
-  templateUrl: './preguntas.component.html',
-  styleUrls: ['./preguntas.component.css']
+    selector: 'app-preguntas',
+    templateUrl: './preguntas.component.html',
+    styleUrls: ['./preguntas.component.css']
 })
 export class PreguntasComponent implements OnInit {
 
-  preguntas: Pregunta[];
-  items: Item[] = new Array();
+    preguntas: Pregunta[];
+    item1: Item;
+    item2: Item;
 
-  constructor(private preguntasService: PreguntasService,
-              private router: Router,
-              private activatedRoute: ActivatedRoute) { }
+    items: Item[] = new Array();
 
-  ngOnInit() {
-    this.activatedRoute.paramMap.subscribe(params => {
-      console.log(params.get('modulo'));
-      const modulo = params.get('modulo');
-      if (modulo) {
-        this.preguntasService.getPreguntas(modulo).subscribe((preguntas) => {
-          this.preguntas = preguntas
-         this.preguntas.forEach(pregunta => {
-            this.items.push({id: pregunta.id, elemento: pregunta.pregunta, mostrar: false});
-            this.items.push({id: pregunta.id, elemento: pregunta.respuesta, mostrar: false});
-          });
+    constructor(private preguntasService: PreguntasService,
+                private router: Router,
+                private activatedRoute: ActivatedRoute) {
+    }
+
+    ngOnInit() {
+        this.activatedRoute.paramMap.subscribe(params => {
+            console.log(params.get('modulo'));
+            const modulo = params.get('modulo');
+            if (modulo) {
+                this.preguntasService.getPreguntas(modulo).subscribe((preguntas) => {
+                    this.preguntas = preguntas
+                    this.preguntas.forEach(pregunta => {
+                        this.items.push({id: pregunta.id, elemento: pregunta.pregunta, mostrar: false, correcto: false});
+                        this.items.push({id: pregunta.id, elemento: pregunta.respuesta, mostrar: false, correcto: false});
+                    });
+                });
+            }
         });
-      }
-    });
-  }
+    }
 
+    public selectItem(event: any, item: Item) {
+        if(this.item1 != null && this.item2 != null) return;
+        if (item.mostrar || item.correcto) {
+            console.log('retrono')
+            return;
+        } else {
+            item.mostrar = true;
+        }
+        if (this.item1 == null) {
+            this.item1 = item;
+        } else {
+            console.log('entro a validar la pareja ')
+            this.item2 = item;
+            if (this.item1.id == this.item2.id) {
+                console.log('la pareja es la misma')
+                this.item1.correcto = true;
+                this.item2.correcto = true;
+                this.items.filter(x => x.id == this.item1.id).forEach(x => {
+                    x.correcto = true
+                });
+                this.item1 = null;
+                this.item2 = null;
+            } else {
+                timer(1000).subscribe(x => { console.log('la pareja no es la misma')
+                    this.items.filter(x => x.id == this.item1.id).forEach(x => {
+                        x.correcto = false;
+                        x.mostrar = false
+                    });
+                    this.items.filter(x => x.id == this.item2.id).forEach(x => {
+                        x.correcto = false;
+                        x.mostrar = false
+                        this.item1 = null;
+                        this.item2 = null;
+                        this.item1 = null;
+                        this.item2 = null;
+                    })});
+            }
 
-
+        }
+    }
 
 }
